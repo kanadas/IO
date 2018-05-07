078f4c921f8fe0b8caacf181e5d9bd9ec815fe61
Dalak <dalak.bartek@gmail.com>
2018-05-07 13:58:29 +0200

    Sending data and checking internet connection

--- src/generate_ga_traffic.py
+++ src/generate_ga_traffic.py
@@ -1,17 +1,59 @@
 import sys
+import requests
 from send_requests_api import send
-from constants import WRONG_DATA, OK, CONNECTION_PROBLEM
+from constants import WRONG_DATA, OK, CONNECTION_PROBLEM, EMPTY_OPTION, WAIT_NEW_SENDING, WAIT_CONTINUE_SENDING,\
+    STOP_SENDING
+
+
+def internet_on():
+    try:
+        requests.get('http://216.58.192.142', timeout=1)
+        return True
+    except (requests.Timeout, requests.ConnectionError):
+        return False
+
+
+def decide():
+    option = EMPTY_OPTION
+
+    while not (option == WAIT_NEW_SENDING or option == WAIT_CONTINUE_SENDING or option == STOP_SENDING):
+        option = int(input("Type:\n1 - Wait for connection and start new sending\n2 - Wait for connection and continue"
+                           " sending\n3 - Stop sending\n"))
+
+    if option == STOP_SENDING:
+        return False, option
+    else:
+        while not internet_on():
+            pass
+
+        print("Connected.")
+
+        return True, option
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         print("Wrong parameters number.\nUsage: python generate_ga_traffic.py tracking_id url vists_no time.")
     else:
-        result = send(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        tracking_id = sys.argv[1]
+        url = sys.argv[2]
+        visits_no = int(sys.argv[3])
+        time = int(sys.argv[4])
+        sending = True
+        all_sent = 0
+        all_time = 0
 
-        if result == OK:
-            print("Successfully send users.")
-        elif result == WRONG_DATA:
-            print("Tracking_id is wrong.")
-        else: #CONNECTION_PROBLEM
-            pass
+        while sending:
+            result, sent_users, sent_time = send(tracking_id, url, visits_no - all_sent, time - all_time)
+            all_sent += sent_users
+            all_time += sent_time
+
+            if result == OK:
+                sending = False
+                print("Successfully send %d/%d users." % (all_sent, visits_no))
+            elif result == WRONG_DATA:
+                sending = False
+                print("Tracking_id is wrong.")
+            else: #CONNECTION_PROBLEM
+                print("Connection problem.")
+                sending, option = decide()
--- src/send_requests_api.py
+++ src/send_requests_api.py
@@ -1,27 +1,53 @@
 import requests
-from constants import WRONG_DATA, OK
+import uuid
+import time
+from constants import WRONG_DATA, OK, CONNECTION_PROBLEM
 
 
 def check_data(tracking_id, url):
-    response = requests.post("https://www.google-analytics.com/debug/collect",
-                             data={"tid": tracking_id, "dp": url, "v": 1, "cid": 1})
+    try:
+        response = requests.post("https://www.google-analytics.com/debug/collect", data={
+            "tid": tracking_id,
+            "dp": url,
+            "v": 1,
+            "cid": 1
+        }, timeout=60)
+    except (requests.Timeout, requests.ConnectionError):
+        return False, CONNECTION_PROBLEM
 
     if response.json()["hitParsingResult"][0]["valid"]:
-        return True
+        return True, OK
     else:
-        return False
+        return False, WRONG_DATA
 
 
+#to narazie zeby sprawdzic czy dziala
 def generate_data(visits_no):
-    pass
+    return [{"agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.1"} for _ in range(visits_no)]
 
 
-def send(tracking_id, url, visits_no, time):
-    result = check_data(tracking_id, url)
+def send(tracking_id, url, visits_no, sending_time):
+    correct, result = check_data(tracking_id, url)
 
-    if result:
+    if correct:
         data = generate_data(visits_no)
-        return OK
+        batch_size = len(data) // sending_time
+
+        for i in range(sending_time):
+            for id, user in enumerate(data[i:i + batch_size]):
+                try:
+                    requests.post("https://www.google-analytics.com/collect", data = {
+                        "v": 1,
+                        "tid": tracking_id,
+                        "cid": uuid.uuid4(),
+                        "dp": url,
+                        "ua": user["agent"] # nie wiem jak beda wygenerowani uzytkownicy
+                    }, timeout=60)
+                except (requests.Timeout, requests.ConnectionError):
+                    return CONNECTION_PROBLEM, i * batch_size + 1 + id, i + 1
+            time.sleep(1)
+
+        return OK, visits_no, sending_time
     else:
-        return WRONG_DATA
+        return result, 0, 0
 
