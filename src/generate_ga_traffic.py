b02d0e337cfce818a5397778fc84f68e705b91e3
Bartosz Gromek <bartoszgromek@eduroam-10-20-4-69.mimuw.edu.pl>
2018-05-09 11:18:37 +0200

    Geneartor prints error msg when users pass negatiove or non integer number of views or seconds

--- src/generate_ga_traffic.py
+++ src/generate_ga_traffic.py
@@ -31,31 +31,50 @@ def decide():
         return True, option
 
 
+def check_if_proper_number(num, msg):
+    is_int = True
+    try:
+        num_int = int(sys.argv[3])
+        if num_int < 0:
+            print("Only positive number of %s! %d" % (msg, num_int))
+            is_int = False
+    except ValueError:
+        is_int = False
+        print("%s has to be an integer!" % msg)
+    return is_int
+
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         print("Wrong parameters number.\nUsage: python generate_ga_traffic.py tracking_id url visits_no time.")
-    else:
-        tracking_id = sys.argv[1]
-        url = sys.argv[2]
+        exit()
+    sending = True
+    tracking_id = sys.argv[1]
+    url = sys.argv[2]
+    if(check_if_proper_number(sys.argv[3], "views")):
         visits_no = int(sys.argv[3])
+    else:
+        exit()
+
+    if(check_if_proper_number(sys.argv[4], "seconds")):
         time = int(sys.argv[4]) * 60
-        sending = True
-        all_sent = 0
+    else:
+        exit()
+    
+    all_sent = 0
+    if visits_no / time > MAX_REQUESTS_PER_MINUTE:
+        print("We can't generate so much visits in that time")
+        exit()
 
-        if visits_no / time > MAX_REQUESTS_PER_MINUTE:
-            print("We can't generate so much visits in that time")
+    while sending:
+        result, sent_users = send(tracking_id, url, visits_no - all_sent, time)
+        all_sent += sent_users
 
-        else:
-            while sending:
-                result, sent_users = send(tracking_id, url, visits_no - all_sent, time)
-                all_sent += sent_users
-
-                if result == OK:
-                    sending = False
-                    print("Successfully sent %d/%d users." % (all_sent, visits_no))
-                elif result == WRONG_DATA:
-                    sending = False
-                    print("Tracking_id is wrong.")
-                else: # CONNECTION_PROBLEM
-                    print("Connection problem.")
-                    sending, option = decide()
+        if result == OK:
+            sending = False
+            print("Successfully sent %d/%d users." % (all_sent, visits_no))
+        elif result == WRONG_DATA:
+            sending = False
+            print("Tracking_id is wrong.")
+        else: # CONNECTION_PROBLEM
+            print("Connection problem.")
+            sending, option = decide()
